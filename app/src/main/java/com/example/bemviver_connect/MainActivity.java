79dac7ba13caba6f75b1ed54cecb8a4fc918b8d8
LucaFigliolaSilvestri <lucaf.silvestri@gmail.com>
2024-04-23 21:30:25 -0300

    Initial commit

---
+++ app/src/main/java/com/example/bemviver_connect/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.bemviver_connect;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void Login (View view)
+    {
+        TextView campoEmail;
+        TextView campoSenha;
+        String email;
+        String senha;
+
+        campoEmail = findViewById(R.id.emailCadastro);
+        campoSenha = findViewById(R.id.senhaCadastro);
+        email = campoEmail.getText().toString();
+        senha = campoSenha.getText().toString();
+
+        campoEmail = findViewById(R.id.emailCadastro);
+        campoSenha = findViewById(R.id.senhaCadastro);
+        email = campoEmail.getText().toString();
+        senha = campoSenha.getText().toString();
+
+        ClasseLogin login1 = new ClasseLogin(email, senha);
+
+        Intent mudarTela = new Intent(getApplicationContext(), cadastro.class);
+        mudarTela.putExtra("objeto", login1);
+        startActivity(mudarTela);
+    }
+
+    public void BotaoCadastro (View view)
+    {
+        Intent mudarTela = new Intent(getApplicationContext(), cadastro.class);
+        startActivity(mudarTela);
+    }
+}
